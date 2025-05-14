52df32853f2ff87f477f9f0ae02746ba3ac4928a
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-05-14 17:06:49 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".e-u-navigation-list");
-  const headerRef = document.querySelector(".e-u-header");
-  const navigationRef = document.querySelector(".e-u-navigation");
+  const navListRef = document.querySelector(".e-i-navigation-list");
+  const headerRef = document.querySelector(".e-i-header");
+  const navigationRef = document.querySelector(".e-i-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigationRef.classList.toggle("is-open");
   });
 
-  document.querySelectorAll(".e-u-navigation-link").forEach((n) =>
+  document.querySelectorAll(".e-i-navigation-link").forEach((n) =>
     n.addEventListener("click", () => {
       navListRef.classList.remove("is-open");
       menuBtnRef.classList.remove("is-open");
@@ -20,47 +20,19 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   );
 });
-document.addEventListener("DOMContentLoaded", () => {
-  const galleryList = document.querySelector(".e-u-gallery-list");
-  const items = galleryList.querySelectorAll(".e-u-gallery-item");
-  const button = document.getElementById("show-more-btn");
-
-  items.forEach((item, index) => {
-    if (index < 3) {
-      item.classList.add("visible");
-    }
-  });
-
-  button.addEventListener("click", () => {
-    items.forEach((item) => item.classList.add("visible"));
-    button.style.display = "none";
-  });
-});
-const images = document.querySelectorAll(".e-u-gallery-image");
-const overlay = document.querySelector(".overlay");
-const overlayImage = document.querySelector(".overlay-image");
-
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    overlayImage.src = image.src;
-    overlay.classList.remove("hidden");
-  });
-});
-
-overlay.addEventListener("click", () => {
-  overlay.classList.add("hidden");
-});
 
 function toggleAnswer(element) {
   const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".e-u-toggle-button img");
+  const buttonImg = element.querySelector(".e-i-toggle-button img");
 
-  if (answer.style.display === "none" || answer.style.display === "") {
-    answer.style.display = "block";
-    buttonImg.style.transform = "rotate(45deg)";
+  answer.classList.toggle("open");
+
+  if (answer.classList.contains("open")) {
+    buttonImg.src = "/img/Minus.png";
+    buttonImg.classList.add("rotated");
   } else {
-    answer.style.display = "none";
-    buttonImg.style.transform = "rotate(90deg)";
+    buttonImg.src = "/img/Add.png";
+    buttonImg.classList.remove("rotated");
   }
 }
 
